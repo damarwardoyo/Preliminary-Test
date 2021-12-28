bf69faa2fec54f656b131fd5e9451e2aae47fb12
damarwardoyo <damarwardoyo@protonmail.com>
2021-12-28 17:09:18 +0700

    inisiasi file untuk masing-masing pertanyaan

---
+++ Find the Dissimilarity.py
@@ -0,0 +1,4 @@
+"""
+Find the difference between 2 string inputs (s and t). Both s and t are inputted by the User. String
+t must be a shuffled string of String s. Return and print the letter that was added to t.
+"""
---
+++ Kulina x Food.py
@@ -0,0 +1,9 @@
+"""
+Given an integer n can be inputted by Users. After the input is inputted, the program will show an
+output in a form of an array of string, which will have the values with conditions:
+● If the current integer is divisible by 3, change the integer to “Kulina” and then insert it
+● If the current integer is divisible by 5, change the integer to “Food” and then insert it
+● If the current integer is divisible by both 3 and 5, change the integer to “Kulina x Food’
+and then insert it
+● Otherwise, insert the integer in string form.
+"""
