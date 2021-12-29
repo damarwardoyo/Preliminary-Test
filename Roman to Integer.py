4ac15370f24200f251fd17c8bd258901acf5ced5
damarwardoyo <damarwardoyo@protonmail.com>
2021-12-29 11:33:04 +0700

    menjawab pertanyaan Roman to Integer

--- Roman to Integer.py
+++ Roman to Integer.py
@@ -19,3 +19,37 @@ There are six instances where subtraction is used:
 ● C can be placed before D (500) and M (1000) to make 400 and 900.
 Given a roman numeral, convert it to an integer.
 """
+
+"""
+angka romawi menggunakan kombinasi dari tujuh simbol yang dimana masing-masing simbol memiliki nilai tertentu. struktur data dictionary dapat mempermudah kita untuk mengetahui 
+nilai dari masing-masing simbol dengan cara menjadikan simbol sebagai key dan nilai dari simbol tersebut sebagai valuenya. cara menghitung angka romawi adalah dengan menjumlahkan 
+nilai dari masing - masing simbol, dimulai dari kiri ke kanan. nilai simbol disebelah kiri biasanya lebih besar atau sama dengan nilai simbol disebelah kanannya, kecuali dalam 
+beberapa kondisi tertentu. untuk memulai mengkonversi angka romawi menjadi integer, kita bisa mengecek masing masing simbol pada string s dari kiri ke kanan. namun ada kondisi 
+khusus yang perlu diperhatikan, apabila nilai simbol pada posisi n lebih besar daripada posisi n-1 maka nilai simbol pada posisi n yang sebenarnya adalah (nilai simbol [n] - nilai simbol [n-1]). 
+oleh karena itu, kita perlu menyimpan nilai simbol yang sudah dibaca ke variabel int prev supaya kita bisa mengecek apakah nilai simbol pada posisi n itu lebih besar daripada posisi n-1. 
+secara default, nilai dari masing-masing simbol karakter ch pada string s akan dijumlah dengan int total. apabila terjadi kondisi dimana nilai simbol n lebih besar daripada nilai 
+simbol n-1, cara mendapatkan nilai total yang sebenarnya adalah dengan menjumlahkan nilai total yang sudah dikoreksi dan nilai simbol pada karakter ch yang sebenarnya. untuk mengetahui 
+nilai simbol pada karakter ch yang sebenarnya, kita mengurangi nilai simbol pada karakter ch dengan nilai karakter yang sebelumnya disimpan pada variabel int prev. lalu untuk mendapat 
+nilai total yang sebenernya, nilai total perlu untuk dikurangi dengan nilai prev karena pada iterasi sebelumnya nilai prev sudah dijumlahkan ke nilai total. 
+secara matematika bisa digambarkan sebagai berikut.
+
+int total = (int total - int prev) + (int curr - int prev); 
+int total = int total + int curr - int prev - int prev;
+int total = int total + int curr - (2*int prev);
+
+fungsi akan mengembalikan nilai int total apabila iterasi sudah selesai.
+"""
+
+def romanToInt(s: str):
+    total = 0
+    romans = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    
+    prev = 0
+    for ch in s:
+        curr = romans[ch]
+        if curr > prev:
+            total += curr - 2 * prev
+        else:
+            total += curr
+        prev = curr
+    return total
